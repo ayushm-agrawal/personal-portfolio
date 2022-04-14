d73638d788628d83ec89c55a82562950434480f9
Ayush Manish Agrawal <aagrawal@nebraska.edu>
2022-04-13 22:34:38 -0500

    :sparkles: introducing blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,67 @@
-import React from "react";
-import '../styles/Blog.css'
+import React, {useState} from "react";
+import '../styles/Blog.css';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {materialDark as CodeStyle} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Markdown from "markdown-to-jsx";
+import BlogItem from "../helpers/BlogText.md";
 
 function Blog() {
+    const CodeBlock = ({className, children}) => {
+        let lang = 'text'; // default monospaced text
+        if (className && className.startsWith('lang-')) {
+            lang = className.replace('lang-', '');
+        }
+        return (
+            <SyntaxHighlighter
+                lineProps={{style: {wordBreak: 'break-all', whiteSpace: 'pre-wrap'}}}
+                wrapLines={true} language={lang} style={CodeStyle}>
+                {children}
+            </SyntaxHighlighter>
+        );
+    }
+
+    const BlockQuote = ({children}) => (
+      <blockquote style={{borderLeft: '3px solid #f6a231'}} >{children}</blockquote>
+    );
+
+    const ImageBlock = ({children, ...rest}) => (
+      <img {...rest} style={{width: '80vw'}} />
+    );
+
+    const Paragraph = ({children}) => (
+        <p style={{marginLeft: '1%', marginRight: '1%'}}>{children}</p>
+    )
+
+    const PreBlock = ({children, ...rest}) => {
+        if ('type' in children && children['type'] === 'code') {
+            return CodeBlock(children['props']);
+        }
+        return <pre {...rest} style={{overflowX: 'scroll'}}>{children}</pre>;
+    };
+
+    const [post, setPost] = useState('');
+
+    fetch(BlogItem)
+        .then(response => response.text())
+        .then(data => setPost(data))
+        .catch(console.error);
+
+
     return (
         <div className='blog'>
-            <h1>This is my blog</h1>
+            <Markdown
+                options={{
+                    overrides: {
+                        pre: PreBlock,
+                        blockquote: BlockQuote,
+                        p: Paragraph,
+                        img: {
+                            component: ImageBlock,
+                        },
+                    },
+                }}>
+                {post}
+            </Markdown>
         </div>
     )
 }
