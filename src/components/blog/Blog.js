483bde34fea5f1a4275ace33456a3873770668ec
Ayush Manish Agrawal <ayush.agrawal7661@gmail.com>
2022-04-18 23:37:09 -0500

    base blog

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,45 +1,12 @@
 import React, {useState} from "react";
 import '../../styles/blog/Blog.css';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {materialDark as CodeStyle} from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Markdown from "markdown-to-jsx";
 import BlogItem from "../../helpers/BlogText.md";
+import {CustomBlockQuote, CustomImageBlock, CustomParagraph, CustomPreBlock} from "./CustomHTMLTagOverride";
+import BlogsList from "./BlogsList";
 
 
 function Blog() {
-    const CodeBlock = ({className, children}) => {
-        let lang = 'text'; // default monospaced text
-        if (className && className.startsWith('lang-')) {
-            lang = className.replace('lang-', '');
-        }
-        return (
-            <SyntaxHighlighter
-                lineProps={{style: {wordBreak: 'break-all', whiteSpace: 'pre-wrap'}}}
-                wrapLines={true} language={lang} style={CodeStyle}>
-                {children}
-            </SyntaxHighlighter>
-        );
-    }
-
-    const BlockQuote = ({children}) => (
-      <blockquote style={{borderLeft: '3px solid #f6a231'}} >{children}</blockquote>
-    );
-
-    const ImageBlock = ({children, ...rest}) => (
-      <img {...rest} style={{width: '80vw'}} />
-    );
-
-    const Paragraph = ({children}) => (
-        <p style={{marginLeft: '1%', marginRight: '1%'}}>{children}</p>
-    )
-
-    const PreBlock = ({children, ...rest}) => {
-        if ('type' in children && children['type'] === 'code') {
-            return CodeBlock(children['props']);
-        }
-        return <pre {...rest} style={{overflowX: 'scroll'}}>{children}</pre>;
-    };
-
     const [post, setPost] = useState('');
 
     fetch(BlogItem)
@@ -50,19 +17,24 @@ function Blog() {
 
     return (
         <div className='blog'>
-            <Markdown
+            {/*<Markdown
                 options={{
                     overrides: {
-                        pre: PreBlock,
-                        blockquote: BlockQuote,
-                        p: Paragraph,
+                        pre: CustomPreBlock,
+                        blockquote: CustomBlockQuote,
+                        p: CustomParagraph,
                         img: {
-                            component: ImageBlock,
+                            component: CustomImageBlock,
                         },
                     },
                 }}>
                 {post}
-            </Markdown>
+            </Markdown>*/}
+            <div className='blog-left'></div>
+            <div className='blog-middle'>
+                <BlogsList/>
+            </div>
+            <div className='blog-right'></div>
         </div>
     )
 }
---
+++ src/components/blog/BlogsListItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function BlogsListItem() {
+    return (
+        <div>
+            <h1>Blog Item</h1>
+            <hr/>
+        </div>
+    )
+}
+
+export default BlogsListItem;
